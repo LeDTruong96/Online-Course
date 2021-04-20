ebc365039ceda0ffebf05f68e2cb517508f6d041
Le Duy Truong <truongld5@truong-iMac.local>
2021-04-20 07:54:49 +0700

    90% clone source code

---
+++ static/app.js
@@ -0,0 +1,9 @@
+document.querySelector('#dark-mode-toggle').onclick = () => {
+	console.log('OK');
+	document.documentElement.classList.toggle('dark');
+};
+
+document.querySelector('#dark-mode-toggle-mb').onclick = () => {
+	console.log('OK');
+	document.documentElement.classList.toggle('dark');
+};
